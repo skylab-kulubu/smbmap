cef0e3292e8c97ca88de2c96f6e23902d1d74adb
Faruk ERDEM <farukomererdem2003@gmail.com>
2025-05-01 01:09:50 +0300

    better table view

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use std::process::exit;
-use pavao::{SmbClient, SmbCredentials, SmbOptions, SmbOpenOptions};
 use clap::Parser;
+use pavao::{SmbClient, SmbCredentials, SmbOpenOptions, SmbOptions};
+use std::process::exit;
 mod parser;
 use crate::parser::Cli;
 #[allow(unused_imports)]
-use log::{
-    error,
-    info,
-    warn,
-};
+use log::{error, info, warn};
+use prettytable::{Cell, Row, Table};
 use std::env;
 fn main() {
     let args = Cli::parse();
@@ -18,53 +15,65 @@ fn main() {
     info!("Target: {}", args.target);
     info!("Port: {}", args.port);
     print_infos(&args);
+    list_shares(&args);
+}
+
+fn list_shares(args: &Cli) {
     match (&args.user, &args.password) {
         (Some(user), Some(password)) => {
-                        let client = SmbClient::new(
-                            SmbCredentials::default()
-                                .server(format!("smb://{}:{}", &args.target, &args.port))
-                                .share("")
-                                .password(password.to_string())
-                                .username(user.to_string()),
-                            SmbOptions::default()
-                            .case_sensitive(true)
-                            .one_share_per_server(true),
-                        )
-                        .unwrap();
-                    let shares = match client.list_dir(""){
-                        Ok(shares) => shares,
-                        Err(e) => {
-                            error!("Error listing shares: {}", e);
-                            exit(1);
-                        }
-                        
-                    };
-                    let spacing = "     ";
-                    print!("Sharename{spacing}|{spacing}Type{spacing}|{spacing}Comment\n");
-                    print!("--------------------------------------\n");
-                    for share in shares {
-                        println!("{:?}{spacing}|{spacing}{:?}{spacing}|{spacing}{:?}", share.name(), share.get_type(), share.comment());
-                    }
-            },
-        (None, None) => info!("No user and password provided, using null user."),
+            let client = SmbClient::new(
+                SmbCredentials::default()
+                    .server(format!("smb://{}:{}", &args.target, &args.port))
+                    .share("")
+                    .password(password.to_string())
+                    .username(user.to_string()),
+                SmbOptions::default()
+                    .case_sensitive(true)
+                    .one_share_per_server(true),
+            )
+            .unwrap();
+            let shares = match client.list_dir("") {
+                Ok(shares) => shares,
+                Err(e) => {
+                    error!("Error listing shares: {}", e);
+                    exit(1);
+                }
+            };
+            let mut table = Table::new();
+            table.add_row(Row::new(vec![
+                Cell::new("Share"),
+                Cell::new("Type"),
+                Cell::new("Comment"),
+            ]));
+            for share in shares {
+                table.add_row(Row::new(vec![
+                    Cell::new(share.name()),
+                    Cell::new(&format!("{:?}", share.get_type())),
+                    Cell::new(share.comment()),
+                ]));
+            }
+            table.printstd();
+        }
+        (None, None) => {
+            info!("No user provided, using null user.");
+        }
         (None, Some(_)) => info!("No user provided, using null user."),
         (Some(_), None) => info!("No password provided, using null user."),
     }
-
 }
 
 fn set_verbosity(args: &Cli) {
     if args.verbose == 0 {
         unsafe {
-        env::set_var("RUST_LOG", "error");
+            env::set_var("RUST_LOG", "error");
         }
-    }else if args.verbose == 1 {
+    } else if args.verbose == 1 {
         unsafe {
-        env::set_var("RUST_LOG", "warn");
+            env::set_var("RUST_LOG", "warn");
         }
-    }else if args.verbose > 2 {
+    } else if args.verbose > 2 {
         unsafe {
-        env::set_var("RUST_LOG", "info");
+            env::set_var("RUST_LOG", "info");
         }
     }
 }
@@ -73,7 +82,7 @@ fn print_infos(args: &Cli) {
     match &args.domain {
         Some(domain) => {
             info!("Domain: {}", domain);
-        },
+        }
         None => {
             info!("No domain provided.");
         }
@@ -82,15 +91,15 @@ fn print_infos(args: &Cli) {
         Some(user) => {
             info!("User: {}", user);
             match &args.password {
-                Some(password ) => {
+                Some(password) => {
                     info!("Password: {}", password);
-                },
+                }
                 None => {
                     error!("No password provided");
                     exit(1)
                 }
             }
-        },
+        }
         None => {
             info!("No user provided, using null user.");
         }
@@ -98,10 +107,9 @@ fn print_infos(args: &Cli) {
     match &args.share {
         Some(share) => {
             info!("Share: {}", share);
-        },
+        }
         None => {
             info!("No share provided, no share enum.");
         }
-    
     }
-    }
+}
